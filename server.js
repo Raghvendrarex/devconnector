ed21673ffc9b457e0d119d524ebcc7cebdb31dff
Raghvendra Singh <raghvendrarex@gmail.com>
2020-12-03 19:44:27 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-const PORT 
+connectDB();
 
-app.listen(PORT);
+app.get("/",(req,res) => res.send("API running!"));
+
+app.use("/api/users",require("./routes/api/users"));
+app.use("/api/auth",require("./routes/api/auth"));
+app.use("/api/profile",require("./routes/api/profile"));
+app.use("/api/post",require("./routes/api/post"));
+
+const PORT = process.env.PORT || 5000; 
+
+app.listen(PORT, () => console.log(`server started on port no ${PORT}` ));
