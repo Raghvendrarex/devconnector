20fcf6b44dbb708da84deaa5d81f204b8b7dba14
Raghvendra Singh <raghvendrarex@gmail.com>
2020-12-11 13:42:00 +0530

    commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,77 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("../../models/User");
+const gravatar = require("gravatar");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const {check,validationResult} = require("express-validator");
 
-router.post('/',(req,res) => res.send("auth routes"));
+
+router.get('/',auth, async (req,res) => {
+    try{
+        
+        const user = await User.findById(req.user.id).select("-password");
+        res.json(user);
+    } catch(err){
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+});
+
+router.post('/',[
+    check("email","Enter A Valid Email").isEmail(),
+    check("password","Password is required").exists()
+],async (req,res) =>{ 
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    const {email,password} = req.body;
+    try {
+        //see if user exists
+            let user = await User.findOne({email});
+
+            if(!user){
+                return res.status(400).json({errors : [ { msg : "Invalid Credentials" } ] });
+            }
+        //get user gravatar
+        const avatar = gravatar.url(email,{
+            s : "200",
+            r : "pg",
+            d :  "robohash"
+        })
+
+        const isMatch = await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(400).json({errors : [ { msg : "Invalid Credentials" } ] });
+        }
+
+        //return json web token
+        const payload = {
+            user:{
+                id:user.id,
+                name:user.name,
+                email:user.email
+            }
+        };
+
+        jwt.sign(payload, config.get("jwtSecret"),{expiresIn:3600000},(err,token)=>{
+            if(err){
+                throw err;
+            }
+            res.json({token});
+        });
+
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server Error");
+    }
+    
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,42 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.post('/',(req,res) => res.send("profile routes"));
+//get api/profile/me
+//get current user profile with private access
+
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'there is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('server error');
+  }
+});
+
+//post api/profile
+//create or update user profile with private access
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'status is required').not().isEmpty(),
+      check('skills', 'skills are required').not().isEmpty()
+    ]
+  ],
+  async (req, res) => {}
+);
 
 module.exports = router;
